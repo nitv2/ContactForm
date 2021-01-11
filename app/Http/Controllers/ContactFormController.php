ddf20b1cf94c7ff14c8781813bbe94905e30ba75
nitv2 <n.t.2846nitv2@gmail.com>
2021-01-12 01:44:39 +0900

    機能追加(Update)

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -130,6 +130,21 @@ class ContactFormController extends Controller
     public function update(Request $request, $id)
     {
         //
+        // $contact = new ContactForm();
+        $contact = ContactForm::find($id);
+        
+        //フォームのyournameを持ってこれるPOST
+        $contact->your_name = $request->input('your_name');
+        $contact->title = $request->input('title');
+        $contact->email = $request->input('email');
+        $contact->url = $request->input('url');
+        $contact->gender = $request->input('gender');
+        $contact->age = $request->input('age');
+        $contact->contact = $request->input('contact');
+
+        $contact->save();
+
+        return redirect('contact/index');
     }
 
     /**
