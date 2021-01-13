29ee1aba3c6b73d3bd1cfd6d1ad89405b00ba3a3
nitv2 <n.t.2846nitv2@gmail.com>
2021-01-14 01:34:31 +0900

    Factoryを用いてダミーデータ生成

--- app/Services/CheckFormData.php
+++ app/Services/CheckFormData.php
@@ -11,29 +11,28 @@ class CheckFormData
             $gender = '女性';
         }
         return $gender;
-    }ß
+    }
 
     public static function checkAge($data){
         switch($data->age){
             case 1:
                 $age ='〜19歳';
-                break;
+                return $age;
             case 2:
                 $age ='20歳〜29歳';
-                break;
+                return $age;
             case 3:
                 $age ='30歳〜39歳';
-                break;
+                return $age;
             case 4:
                 $age ='40歳〜49歳';
-                break;
+                return $age;
             case 5:
                 $age ='50歳〜59歳';
-                break; 
-            case 6:
+                return $age;
+            default:
                 $age ='60歳〜';
-                break;
+                return $age; 
         }
-        return $age;
     }
 }
